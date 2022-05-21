11c5bfbc7981a376bb75f64186dd4319ed46a902
Giglium <fre095@gmail.com>
2022-05-21 09:44:44 +0200

    Starting

--- formatter/fmt.mk
+++ formatter/fmt.mk
@@ -7,3 +7,7 @@
 .PHONY: fmt.black
 fmt.black: #! Run Black as Python standard formatter on the project folder.
 	black $(ROOT)
+
+.PHONY: fmt.black
+fmt.isort: #! Run isort as Python standard formatter on the project folder.
+	isort $(ROOT)
